11be2abd887f7ffeb4fa42a0112f7570f465110d
grezde <cbavbz@gmail.com>
2020-06-21 13:52:45 +0300

    bootstrapul pulii

--- source/index.js
+++ source/index.js
@@ -1,20 +1,16 @@
 
 window.onload = function() {
 
-    var container = document.getElementById('container');
+    var container = document.getElementById('all_problems');
     
     getAll([[getJson, 'data/ipho'], [getFile, 'bits/ipho_year.bit']], function(data) {
         
-        container.innerHTML = populate(data[1][0], { values: data[0][0] });
+        container.innerHTML = populate(data[1][0], {
+            values: data[0][0],
+            base: 'https://www.ioc.ee/~kree/students/iphoTable/'
+        }, null);
         console.log(container.innerHTML);
 
-    });
-
-    getJson('data/ipho', function(data) {
-        for(var year of data) {
-            var str = '<h2>' + year.year + ' - ' + year.country + '</h2>';
-            container.innerHTML += str;
-        }
     });
 
 };
--- source/population.js
+++ source/population.js
@@ -1,55 +1,68 @@
 
-
-function populate(text, obj) {
-    console.log('POPULATING', text, obj);
-
-    var result = '';
+function separate(text) {
     var data = text.split('$');
-    result += data.shift();
+    var first = data.shift();
     data = data.map(function(dataEl) {
-        var sep = dataEl.split(' ');
-        var replname = sep.shift();
-        var rest = ' ' + sep.join(' ');
+        var size = dataEl.split(/[^a-zA-Z:?]/, 1)[0].length;
+        var replname = dataEl.slice(0, size);
+        var rest = dataEl.slice(size);
         return [replname, rest];
     });
-    console.log('OBTAINED', result, data);
-    for(var i=0; i<data.length; i++) {
+    data.unshift(['', first]);
+    return data;
+}
+
+function getValue(name, obj, context) {
+    if(name in obj)
+        return obj[name];
+    if(name in context)
+        return context[name];
+    return undefined;
+}
+
+function populate(text, obj, context) {
+
+    if(context == null)
+        context = obj;
+
+    var result = '';
+    var data = separate(text);
+    result += data[0][1];
+
+    console.log(data);
+
+    for(var i=1; i<data.length; i++) {
         
-        if(data[i][0][0] == ':') {
-            var ini = i;
-            var arrat = obj[data[i][0].slice(1)];
+        var ini=i, nextObj, toBePopulated='';
+        if(':?!'.includes(data[i][0][0])) {
             i++;
             while(data[i][0] != data[ini][0])
                 i++;
-            
-            var toBePopulated = data[ini][1];
+
+            toBePopulated += data[ini][1];
             for(var j=ini+1; j<i; j++)
                 toBePopulated += '$'+data[j][0]+data[j][1];
 
-            for(var j=0; j<arrat.length; j++)
-                result += populate(toBePopulated, arrat[j]);
+            nextObj = getValue(data[i][0].slice(1), obj, context);            
+        }
 
-            result += data[i][1];
+        if(data[ini][0][0] == ':') {
+            for(var j=0; j<nextObj.length; j++)
+                result += populate(toBePopulated, nextObj[j], context);
         }
         else if(data[i][0][0] == '?') {
-            var ini = i;
-            var exists = obj[data[i][0].slice(1)];
-            i++;
-            while(data[i][0] != data[ini][0])
-                i++;
-            
-            var toBePopulated = data[ini][1];
-            for(var j=ini+1; j<i; j++)
-                toBePopulated += '$'+data[j][0]+data[j][1];
-
-            if(exists)
-                result += populate(toBePopulated, obj);
-            result += data[i][1];
+            if(nextObj)
+                result += populate(toBePopulated, obj, context);
+        }
+        else if(data[i][0][0] == '!') {
+            if(!nextObj)
+                result += populate(toBePopulated, obj, context);
         }
         else
-            result += obj[data[i][0]] + data[i][1];
+            result += getValue(data[i][0], obj, context);
+        result += data[i][1];
         
     }
-    console.log('FINISHED', result);
     return result;
 }
+
