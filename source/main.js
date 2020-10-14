dd6311e2135aaf2de44309bb9cc2e91bbffa0bea
grezde <cbavbz@gmail.com>
2020-10-14 20:30:15 +0300

    adaugat sectiuni

--- source/main.js
+++ source/main.js
@@ -1,5 +1,6 @@
 
 var container, contestsContainer;
+var siteSections = [], selectedSection=1;
 var contests, selected=0;
 var classFiltersEl, filtersEl;
 
@@ -15,11 +16,25 @@ function rerender() {
     else 
         document.getElementById('classFilters').style.display = 'none'; 
 
-    document.getElementById('longName').textContent = '(' + contests[selected].longName + ')';
+    //document.getElementById('longName').textContent = '(' + contests[selected].longName + ')';
     contests[selected].data.jaanNotes = document.getElementById('showJaan').checked;
     contests[selected].data.showTopics = document.getElementById('showTopics').checked;
     container.innerHTML = populate(contests[selected].template, contests[selected].data, null); 
     makeVisible();
+}
+
+function reselect() {
+    for(var i=0; i<siteSections.length; i++)
+        if(Array.from(siteSections[i].element.classList).includes('active')) {
+            selectedSection = i;
+            break;
+        }
+ 
+    for(var i=0; i<siteSections.length; i++) {
+        siteSections[i].idsToShow.forEach(function(id) {
+            document.getElementById(id).style.display = selectedSection == i ? 'initial' : 'none'
+        });
+    }
 }
 
 window.onload = function() {
@@ -35,6 +50,16 @@ window.onload = function() {
         contestsContainer.innerHTML = populate(data[1][0], data[0][0]);
         for(var i=0; i<contests.length; i++)
             contests[i].element = document.getElementById('contest'+(i+1));
+        for(var i=0; i<3; i++) {
+            siteSections[i] = {
+                element: document.getElementById('section'+(i+1))
+            };
+        }
+        siteSections[0].idsToShow = ['account_info'];
+        siteSections[1].idsToShow = ['all_problems', 'archive_container'];
+        siteSections[2].idsToShow = ['small_contests'];
+        reselect();
+
         var jsonArray = [getJson];
         var temArray = [getFile], temMap=[];
         for(var i=0; i<contests.length; i++) {
@@ -51,6 +76,7 @@ window.onload = function() {
         getAll([jsonArray, temArray], function(data) {
             for(var i=0; i<contests.length; i++) {
                 contests[i].data = data[0][i];
+                contests[i].data.longName = contests[i].longName;
                 contests[i].template = data[1][temMap[i]];
             }
             rerender();
