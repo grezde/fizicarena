3798b609c106c7e8453ebc7c5835673198561a7c
grezde <cbavbz@gmail.com>
2020-10-20 17:59:06 +0300

    added basic suport for experimental problems

--- source/main.js
+++ source/main.js
@@ -1,10 +1,11 @@
 
-var container, contestsContainer;
+var container, contestsContainer, probaToggle;
 var siteSections = [], selectedSection=1;
-var contests, selected=0;
+var contests, selected=0, selectedProba=0;
 var classFiltersEl, filtersEl;
 
 function rerender(newIndex) {
+    selectedProba = probaToggle.children[0].classList.contains('active') ? 0 : 1;
     for(var i=0; i<contests.length; i++)
         if(Array.from(contests[i].element.parentNode.classList).includes('active')) {
             selected = i;
@@ -19,12 +20,32 @@ function rerender(newIndex) {
     if(contests[selected].hasClass)
         document.getElementById('classFilters').style.display = 'initial'; 
     else 
-        document.getElementById('classFilters').style.display = 'none'; 
+        document.getElementById('classFilters').style.display = 'none';
 
-    contests[selected].data.jaanNotes = document.getElementById('showJaan').checked;
-    contests[selected].data.showTopics = document.getElementById('showTopics').checked;
-    contests[selected].data.showState = document.getElementById('showState').checked;
-    container.innerHTML = populate(contests[selected].template, contests[selected].data, null); 
+    for(var i=0; i<contests.length; i++)
+        if(selectedProba == 0) {
+            contests[i].element.parentElement.classList.remove('disabled');
+            contests[i].element.disabled = false;
+        } else if(!contests[i].experimental) {
+            contests[i].element.parentElement.classList.add('disabled');
+            contests[i].element.disabled = true;
+        }
+    if(selectedProba == 1 && !contests[selected].experimental) {
+        contests[selected].element.parentNode.classList.remove('active');
+        contests[0].element.parentNode.classList.add('active');
+        selected = 0;
+    }
+
+    var con;
+    if(selectedProba == 0)
+        con = contests[selected];
+    else
+        con = contests[selected].experimental;
+
+    con.data.jaanNotes = document.getElementById('showJaan').checked;
+    con.data.showTopics = document.getElementById('showTopics').checked;
+    con.data.showState = document.getElementById('showState').checked;
+    container.innerHTML = populate(con.template, con.data, null); 
     makeVisible();
 
 }
@@ -56,6 +77,7 @@ function reselect(newIndex) {
 
 window.onload = function() {
 
+    probaToggle = document.getElementById('proba_toggle');
     container = document.getElementById('all_problems');
     contestsContainer = document.getElementById('contests_container');
     classFiltersEl = document.getElementById('classFilters');
@@ -65,7 +87,7 @@ window.onload = function() {
     getAll([[getJson, 'data/contests'], [getFile, 'templates/selectContest.html', 'templates/accountInfo.html']], function(data) {
         contests = data[0][0].contests;
         for(var i=0; i<contests.length; i++)
-            contests[contests[i].data] = contests[i];
+            contests[contests[i].shortName] = contests[i];
 
         contestsContainer.innerHTML = populate(data[1][0], data[0][0]);
         for(var i=0; i<contests.length; i++)
@@ -85,23 +107,42 @@ window.onload = function() {
         var jsonArray = [getJson];
         var temArray = [getFile], temMap=[];
         for(var i=0; i<contests.length; i++) {
-            var tempStr = 'templates/'+contests[i].template+'.html';
+            var tempStr = 'templates/archive/'+contests[i].template+'.html', tindex;
             jsonArray.push('data/'+contests[i].data);
-            
+            if(contests[i].experimental)
+                jsonArray.push('data/'+contests[i].experimental.data);
+
             if(temArray.includes(tempStr))
-                temMap.push(temArray.indexOf(tempStr)-1);
+                temMap.push(tindex = temArray.indexOf(tempStr)-1);
             else {
-                temMap.push(temArray.length-1);
+                temMap.push(tindex = temArray.length-1);
                 temArray.push(tempStr);
+            }
+            if(contests[i].experimental) {
+                if(contests[i].experimental.template) {
+                    tempStr = 'templates/archive/'+contests[i].experimental.template+'.html';
+                    if(temArray.includes('archive/'+tempStr))
+                        temMap.push(temArray.indexOf(tempStr)-1);
+                    else {
+                        temMap.push(temArray.length-1);
+                        temArray.push('archive/'+tempStr);
+                    }
+                } else 
+                    temMap.push(tindex);
             }
         }
         getAll([jsonArray, temArray], function(data) {
-            for(var i=0; i<contests.length; i++) {
-                contests[i].shortName = contests[i].data;
-                contests[i].data = data[0][i];
+            for(var i=0, j=0; i<contests.length; i++) {
+                contests[i].data = data[0][j];
+                contests[i].template = data[1][temMap[j++]];
                 contests[i].data.longName = contests[i].longName;
                 contests[i].data.shortName = contests[i].shortName;
-                contests[i].template = data[1][temMap[i]];
+                if(contests[i].experimental) {
+                    contests[i].experimental.data = data[0][j];
+                    contests[i].experimental.template = data[1][temMap[j++]];
+                    contests[i].experimental.data.longName = contests[i].experimental.longName = contests[i].longName;
+                    contests[i].experimental.data.shortName = contests[i].experimental.shortName;
+                }  
             }
             getData();
             rerender();
